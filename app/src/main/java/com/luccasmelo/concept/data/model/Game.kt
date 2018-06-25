0d9bb195d94392992fc7011c8e195f9ed886a2bd
luccasMelo <luccas.melo97@gmail.com>
2018-06-25 18:03:27 -0300

    Model

--- app/src/main/java/com/luccasmelo/concept/data/model/Game.kt
+++ app/src/main/java/com/luccasmelo/concept/data/model/Game.kt
@@ -1,7 +1,30 @@
 package com.luccasmelo.concept.data.model
 
+import com.google.gson.annotations.SerializedName
+
 public class Game(val name: String, val popularity: Int, val localized_name: String) {
     lateinit var box: Box
 }
 
-public class Box(val template: String)
+public class GameContainer(public val game:Game){
+    lateinit var viewers:String
+    lateinit var channels:String
+
+    override fun equals(other: Any?): Boolean {
+        return hashCode() == (other as GameContainer).hashCode()
+    }
+
+    override fun hashCode(): Int {
+        var result = game.hashCode()
+        result = 31 * result + viewers.hashCode()
+        result = 31 * result + channels.hashCode()
+        result = 31 * result + game.name.hashCode()
+        return result
+    }
+
+
+}
+
+public class GameResponse(val top:List<GameContainer>)
+
+public class Box(@SerializedName("large") val url: String)
