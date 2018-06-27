c3ed18395b13df638c20c77c46f9cf858bd551ff
luccasMelo <luccas.melo97@gmail.com>
2018-06-27 13:00:22 -0300

    Utils

--- app/src/main/java/com/luccasmelo/concept/utils/Constants.kt
+++ app/src/main/java/com/luccasmelo/concept/utils/Constants.kt
@@ -1,6 +1,13 @@
 package com.luccasmelo.concept.utils
 
 object Constants{
-       const val ENDPOINT = "https://api.twitch.tv/kraken/games/top"
+       const val ENDPOINT = "https://api.twitch.tv/kraken/"
+       const val ICON_VIEWER = "iconViewers"
+       const val ICON_CHANNEL = "iconChannels"
+       const val SHARED_ELEMENT_ID = "game_image"
+       const val GAME_URL_EXTRA = "url"
+       const val GAME_NAME_EXTRA = "name"
+
+
 
 }
